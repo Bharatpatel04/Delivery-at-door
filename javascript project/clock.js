9170a28ac37a17e119a3b9a810c6881102b265dd
Bharat Patel <bharat844081@gmail.com>
2023-05-28 13:03:23 +0530

    projec files are added

---
+++ javascript project/clock.js
@@ -0,0 +1,8 @@
+ 
+ var date = new Date();
+ 
+var h = date.getHours();
+var m = date.getMinutes();
+var s = date.getSeconds();
+
+document.getElementById("DigitalClock").innerHTML= h + ":" + m ":" + s;
---
+++ javascript project/switch.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+ int main()
+{
+	int a,b;
+	printf("select no.\n");
+	scanf("%d",&a);
+	printf("select no.\n");
+	scanf("%d",&b);
+	return 0;
+}
